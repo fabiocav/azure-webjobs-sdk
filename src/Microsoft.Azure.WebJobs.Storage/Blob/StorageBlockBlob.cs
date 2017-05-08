2d5a4076daf2cade8fbe817d33dd3905f984dde2
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-05-08 14:49:32 -0700

    Updating storage

--- src/Microsoft.Azure.WebJobs.Storage/Blob/StorageBlockBlob.cs
+++ src/Microsoft.Azure.WebJobs.Storage/Blob/StorageBlockBlob.cs
@@ -57,12 +57,6 @@ namespace Microsoft.Azure.WebJobs.Host.Storage.Blob
         }
 
         /// <inheritdoc />
-        public Uri Uri
-        {
-            get { return _sdk.Uri; }
-        }
-
-        /// <inheritdoc />
         public string Name
         {
             get { return _sdk.Name; }
@@ -90,43 +84,43 @@ namespace Microsoft.Azure.WebJobs.Host.Storage.Blob
         public Task<string> AcquireLeaseAsync(TimeSpan? leaseTime, string proposedLeaseId,
             CancellationToken cancellationToken)
         {
-            return _sdk.AcquireLeaseAsync(leaseTime, proposedLeaseId, cancellationToken);
+            return _sdk.AcquireLeaseAsync(leaseTime, proposedLeaseId, accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task DeleteAsync(CancellationToken cancellationToken)
         {
-            return _sdk.DeleteAsync(cancellationToken);
+            return _sdk.DeleteAsync(DeleteSnapshotsOption.None, accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task<string> DownloadTextAsync(CancellationToken cancellationToken)
         {
-            return _sdk.DownloadTextAsync(cancellationToken);
+            return _sdk.DownloadTextAsync(encoding: null, accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task<bool> ExistsAsync(CancellationToken cancellationToken)
         {
-            return _sdk.ExistsAsync(cancellationToken);
+            return _sdk.ExistsAsync(options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task FetchAttributesAsync(CancellationToken cancellationToken)
         {
-            return _sdk.FetchAttributesAsync(cancellationToken);
+            return _sdk.FetchAttributesAsync(accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task<Stream> OpenReadAsync(CancellationToken cancellationToken)
         {
-            return _sdk.OpenReadAsync(cancellationToken);
+            return _sdk.OpenReadAsync(accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
         public Task<CloudBlobStream> OpenWriteAsync(CancellationToken cancellationToken)
         {
-            return _sdk.OpenWriteAsync(cancellationToken);
+            return _sdk.OpenWriteAsync(accessCondition: null, options: null, operationContext: null, cancellationToken: cancellationToken);
         }
 
         /// <inheritdoc />
@@ -141,7 +135,7 @@ namespace Microsoft.Azure.WebJobs.Host.Storage.Blob
             OperationContext operationContext, CancellationToken cancellationToken)
         {
             return _sdk.RenewLeaseAsync(accessCondition, options, operationContext, cancellationToken);
-        } 
+        }
 
         /// <inheritdoc />
         public Task SetMetadataAsync(AccessCondition accessCondition, BlobRequestOptions options,
