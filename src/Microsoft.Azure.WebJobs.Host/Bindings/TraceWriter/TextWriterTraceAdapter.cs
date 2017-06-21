5fef6edb07194f8ed48d60eabd5ad5cd750386d9
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-06-20 17:27:24 -0700

    Update to trace adapter test

--- src/Microsoft.Azure.WebJobs.Host/Bindings/TraceWriter/TextWriterTraceAdapter.cs
+++ src/Microsoft.Azure.WebJobs.Host/Bindings/TraceWriter/TextWriterTraceAdapter.cs
@@ -58,7 +58,7 @@ namespace Microsoft.Azure.WebJobs.Host.Bindings
                 return false;
             }
 
-            for (int i = 1; i < Environment.NewLine.Length; i++)
+            for (int i = 1; i <= Environment.NewLine.Length; i++)
             {
                 if (_text[_text.Length - i] != Environment.NewLine[Environment.NewLine.Length - i]) 
                 {
