756dd4dd8ecd3f3d1401fcfce5dbd36a7068db72
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2017-11-01 19:47:38 -0700

    Initial Hosting/DI model implementation

--- sample/SampleHost/Program.cs
+++ sample/SampleHost/Program.cs
@@ -5,30 +5,37 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Logging;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace SampleHost
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var config = new JobHostConfiguration();
-            config.Queues.VisibilityTimeout = TimeSpan.FromSeconds(15);
-            config.Queues.MaxDequeueCount = 3;
-            config.LoggerFactory = new LoggerFactory().AddConsole();
+            var builder = JobHostBuilder.CreateDefault()
+                .UseConsoleLifetime();
 
-            if (config.IsDevelopment)
-            {
-                config.UseDevelopmentSettings();
-            }
+            var jobHost = builder.Build();
 
-            config.UseEventHub();
-            config.UseServiceBus();
+            await jobHost.RunAsync();
 
-            CheckAndEnableAppInsights(config);
+            //var config = new JobHostConfiguration();
+            //config.Queues.VisibilityTimeout = TimeSpan.FromSeconds(15);
+            //config.Queues.MaxDequeueCount = 3;
+            //config.LoggerFactory = new LoggerFactory().AddConsole();
 
-            var host = new JobHost(config);
-            host.RunAndBlock();
+            //if (config.IsDevelopment)
+            //{
+            //    config.UseDevelopmentSettings();
+            //}
+
+            //CheckAndEnableAppInsights(config);
+
+            //var host = new JobHost(config);
+            //host.RunAndBlock();
         }
 
         private static void CheckAndEnableAppInsights(JobHostConfiguration config)
---
+++ src/Microsoft.Azure.WebJobs.Host/Hosting/JobHostService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.WebJobs.Host.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Hosting
+{
+    public class JobHostService : IHostedService
+    {
+        private readonly ILogger<JobHostService> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly JobHost _jobHost;
+
+        public JobHostService(IConfiguration configuration, IOptions<JobHostOptions> jobHostOptions, ILogger<JobHostService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _configuration = configuration;
+            _jobHost = new JobHost();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            _jobHost.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => _jobHost.StartAsync(cancellationToken);
+    }
+}
